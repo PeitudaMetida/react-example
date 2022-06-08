16709e646591d3af4f2547db141c0717afa5fc78
Talulah23 <piepo.andressa@gmail.com>
2022-06-08 16:51:54 -0300

    Alterei parte do cadastro

--- src/pages/cadastroPage/index.tsx
+++ src/pages/cadastroPage/index.tsx
@@ -1,10 +1,13 @@
 import { Center } from "@chakra-ui/react";
-import { Spacer, Box, FormLabel, Input, Button, Spinner } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/Auth";
+import { Container, Button, Label, Input, Container2 } from "./styles";
 
-function LoginPage() {
+function CadastroPage() {
+    
+    const nameRef = useRef<HTMLInputElement | null>(null);
     const emailRef = useRef<HTMLInputElement | null>(null);
     const passwordRef = useRef<HTMLInputElement | null>(null);
     const navigate = useNavigate();
@@ -24,23 +27,31 @@ function LoginPage() {
     
     return (
         isUserDataPresent ? 
-            <Box m="25px">
-                <form onSubmit={handleOnSubmit}>
-                    <FormLabel htmlFor="email"><b>Username</b></FormLabel>
-                    <Input ref={emailRef} type="email" placeholder="Enter email" name="email" required/>
-                    <Spacer/>
-                    <FormLabel htmlFor="psw"><b>Password</b></FormLabel>
-                    <Input ref={passwordRef} type="password" placeholder="Enter Password" name="psw" required/>
-                    <Box display='flex' mt="5px">
-                        <Button colorScheme={"blue"} type="submit">Login</Button>
-                        <Spacer></Spacer>
-                        <Button colorScheme={"green"} onClick={() => signIn("google")} type="button">Login com google</Button>
-                    </Box>
+            <Container>
+                <form onSubmit={handleOnSubmit} className="principal2">
+                    <Label  htmlFor="name"><b>Nome</b></Label>
+                    <br />
+                    <Input ref={nameRef} type="name" placeholder="Insira seu nome" name="name" required/>
+                    <br/>
+                    <br />
+                    <Label  htmlFor="email"><b>E-mail</b></Label>
+                    <br />
+                    <Input ref={emailRef} type="email" placeholder="Insira seu email" name="email" required/>
+                    <br/>
+                    <br />
+                    <Label htmlFor="psw"><b>Password</b></Label>
+                    <br />
+                    <Input ref={passwordRef} type="password" placeholder="Insira a senha" name="psw" required/>
+                    <br />
+                    <br />
+                    <Container2>
+                        <Button>Cadastre-se</Button>
+                    </Container2>
                 </form>
-            </Box>
+            </Container>
         :
         <Center><Spinner/></Center>
     );
 }
 
-export default LoginPage;
+export default CadastroPage;
