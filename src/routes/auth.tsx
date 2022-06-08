1b7465e4254b4d44565df6c5bb9a1d7289be61cc
Talulah23 <piepo.andressa@gmail.com>
2022-06-08 15:24:52 -0300

    Inseri rota do cadastro

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -3,10 +3,12 @@ import {
   Route,
 } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
+import CadastroPage from "../pages/cadastroPage";
 function VehicleRoutes() {
   return (
     <Routes>
         <Route path="/" element={<LoginPage />}/>
+        <Route path="/cadastro" element={<CadastroPage />}/>
     </Routes>
   )
 }
